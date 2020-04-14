2d83de05f0a054e678096f91b62fcfcd8ad9b151
Marisol <marisol.castro@ioconnectservices.com>
2020-04-14 17:56:58 -0500

    Put and delete event added to todos with http call request to api

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../../services/todo.service'
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.scss'],
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+
+  constructor(private todoService:TodoService) {}
+
+  ngOnInit(): void {}
+
+  //Set dynamic classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed,
+    };
+    return classes;
+  }
+
+  //onToggle
+  onToggle(todo) {
+    //Toggle in UI
+    console.log('toggling...');
+    todo.completed = !todo.completed;
+    //Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo =>
+      console.log(todo));
+  }
+
+  //onDelete
+  onDelete(todo) {
+    this.deleteTodo.emit(todo);
+  }
+}
